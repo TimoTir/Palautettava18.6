11f2341deeded1a139fc84774b1ed16bd09cca6a
Timo Tirkkonen <timo.tirkkonen@student.careeria.fi>
2019-06-12 13:52:08 +0300

    Muutoksia 12.6 Logins

---
+++ WebAppPalautus/WebAppPalautus/Controllers/HomeController.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebAppPalautus.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            if (Session["UserName"] == null)
+            {
+                ViewBag.LoggedStatus = "Out";
+                return RedirectToAction("login", "home");
+            }
+            else
+            {
+                ViewBag.LoggedStatus = "In";
+                return View();
+            }
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "About";
+
+            if (Session["UserName"] == null)
+            {
+                ViewBag.LoggedStatus = "Out";
+                return RedirectToAction("login", "home");
+            }
+            else
+            {
+                ViewBag.LoggedStatus = "In";
+
+                ViewBag.Message = "Yhtiön perustietojen kuvailua";
+                ViewBag.Herja = "Ole huolellinen, niin ei tule virhettä :D";
+
+                return View();
+
+            }
+        }
+    
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "contact.";
+
+            if (Session["UserName"] == null)
+            {
+                ViewBag.LoggedStatus = "Out";
+                return RedirectToAction("login", "home");
+            }
+            else
+            {
+                ViewBag.LoggedStatus = "In";
+                ViewBag.Message = "Contact";
+
+                return View();
+            }
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Authorize(Logins LoginModel)
+        {
+            tilausdbEntities db = new tilausdbEntities();
+            //Haetaan käyttäjän/Loginin tiedot annetuilla tunnustiedoilla tietokannasta LINQ -kyselyllä
+            var LoggedUser = db.Logins.SingleOrDefault(x => x.UserName == LoginModel.UserName && x.PassWord == LoginModel.PassWord);
+            if (LoggedUser != null)
+            {
+                ViewBag.LoginMessage = "Successfull login";
+                ViewBag.LoggedStatus = "In";
+                Session["UserName"] = LoggedUser.UserName;
+                return RedirectToAction("Index", "Home"); //Tässä määritellään mihin onnistunut kirjautuminen johtaa --> Home/Index
+            }
+            else
+            {
+                ViewBag.LoginMessage = "Login unsuccessfull";
+                ViewBag.LoggedStatus = "Out";
+                LoginModel.LoginErrorMessage = "Tuntematon käyttäjätunnus tai salasana.";
+                return View("Login", LoginModel);
+            }
+        }
+    }
+}
---
+++ WebAppPalautus/WebAppPalautus/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebAppPalautus.MvcApplication" Language="C#" %>
